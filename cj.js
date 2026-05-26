c570210e0cc4c6b03fdd8b5f92f3994287b03a0b
Page <424005731@ntc.edu.ph>
2026-05-26 14:11:20 +0800

    Create cj.js

---
+++ cj.js
@@ -0,0 +1,21 @@
+function showMessage() {
+  alert("Thank you for choosing Nanay's Karinderia!");
+}
+
+function changeColor() {
+
+  document.body.style.backgroundColor = "#fff3cd";
+
+}
+
+function welcomeAbout() {
+
+  alert("We serve Filipino comfort food with love!");
+
+}
+
+function showDiscount() {
+
+  alert("Promo Today: Buy 2 meals and get free iced tea!");
+
+}
